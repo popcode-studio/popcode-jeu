b0e3e6e75d3ed69bddf2e2b3e50b379ddc39dc86
popcode-studio <popcodestudio29@gmail.com>
2026-04-06 02:14:36 +0200

    feat: add Puissance4 to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import Morpion from './games/morpion/Morpion'
+import Puissance4 from './games/puissance4/Puissance4'
 
 function App() {
   return (
     <div style={{ display: 'flex', justifyContent: 'center', marginTop: '50px' }}>
-      <Morpion />
+      <Puissance4 />
     </div>
   )
 }
